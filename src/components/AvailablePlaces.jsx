92f2716edc0fb605cc2fc32dba47ee53e8332119
sohail <sohail2001rana@gmail.com>
2026-05-11 10:51:18 +0530

    Enhance error handling and loading state in AvailablePlaces component

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,22 +1,50 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import ErrorPage from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isLoading, setIsLoading] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/placess");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places!");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({ message: error.message || "Something went wrong!" });
+      }
+
+      setIsLoading(false);
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return (
+      <ErrorPage
+        title="An error occurred!"
+        message={error.message}
+        onConfirm={() => setError(null)}
+      />
+    );
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Loading available places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
